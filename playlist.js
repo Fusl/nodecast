7eb978c45b90a4221499f89c566a2c1f4fcf6c0b
Fusl <root@hallowe.lt>
2013-11-21 04:19:24 +0100

    2013.11.21, Version 0.0.5 (Beta)
    - transcoder.js
    * Bugfix~: Default-Codec "libfaac" is an alias to aac but not on all systems out there
    * Bugfix~: Default-Retrywait should be really low to prevent underflow in the buffer
    
    - dispatcher.js
    * Bugfix: clients now don't get too many listeners caused by a typo in the overflow-check
    * Bugfix: process does not crash anymore by serving /listen.pls when mounts aren't setup completely
    * Moved init-starts from anywhere between function definitions to the bottom
    
    - functions.js
    * Initial commit (moved often called functions from all js files into there)

--- playlist.js
+++ playlist.js
@@ -107,8 +107,6 @@ var playlists = {
 
 var functions = require('./functions.js');
 var child_process = functions.child_process;
-var exec = child_process.exec;
-var spawn = child_process.spawn;
 var fs = functions.fs;
 var http = functions.http;
 var in_array = functions.in_array;
@@ -116,105 +114,122 @@ var log = functions.log;
 var os = functions.os;
 var path = functions.path;
 var shuffle = functions.shuffle;
-var tmp = {};
 var uniqid = functions.uniqid;
 var util = functions.util;
 
-var rescan = function() {
-    var newtmp = {};
-    Object.keys(playlists).forEach(function(playlistkey) {
-        if(typeof playlists[playlistkey]._ !== 'object') {
+var rescan = function () {
+    Object.keys(playlists).forEach(function (playlistkey) {
+        if (typeof playlists[playlistkey]._ !== 'object') {
             playlists[playlistkey]._ = {};
-        };
-        Object.keys(playlists[playlistkey].directorymaps).forEach(function(directorymapkey) {
-            findfiles(playlists[playlistkey].directorymaps[directorymapkey], function(files) {
+        }
+        Object.keys(playlists[playlistkey].directorymaps).forEach(function (directorymapkey) {
+            findfiles(playlists[playlistkey].directorymaps[directorymapkey], function (files) {
                 playlists[playlistkey]._[directorymapkey] = files;
             });
         });
     });
 };
 
-var findfiles = function(dir, callback) {
-    exec('find "'+dir+'" -type f', function(error, stdout, stderr) {
-        var files = new Array();
-        stdout.split("\n").forEach(function(file, fileIndex) {
-            if(file.trim() !== '') {
+var findfiles = function (dir, callback) {
+    child_process.exec('find "' + dir + '" -type f', function (error, stdout, stderr) {
+        var files = [];
+        stdout.split("\n").forEach(function (file, fileIndex) {
+            if (file.trim() !== '') {
                 files.push(file);
-            };
+            }
         });
-        if(typeof callback === 'function') {
+        if (typeof callback === 'function') {
             callback(files);
-        };
+        }
     });
 };
 
-var getrandfile = function(playlistkey) {
+var getrandfile = function (playlistkey) {
     var directorymapkey = false;
-    if(typeof playlists[playlistkey].calendarmaps[(new Date()).getHours()] === 'string') {
+    if (typeof playlists[playlistkey].calendarmaps[(new Date()).getHours()] === 'string') {
         directorymapkey = playlists[playlistkey].calendarmaps[(new Date()).getHours()];
-    } else if(typeof playlists[playlistkey].calendarmaps['fallback'] === 'string') {
-        directorymapkey = playlists[playlistkey].calendarmaps['fallback'];
-    } else if(typeof playlists[playlistkey].calendarmaps['default'] === 'string') {
+    } else if (typeof playlists[playlistkey].calendarmaps.falback === 'string') {
+        directorymapkey = playlists[playlistkey].calendarmaps.fallback;
+    } else if (typeof playlists[playlistkey].calendarmaps['default'] === 'string') {
         directorymapkey = playlists[playlistkey].calendarmaps['default'];
-    };
-    if(typeof playlists[playlistkey]._ === 'object') {
-        if(typeof playlists[playlistkey]._[directorymapkey] === 'object') {
+    }
+    if (typeof playlists[playlistkey]._ === 'object') {
+        if (typeof playlists[playlistkey]._[directorymapkey] === 'object') {
             return shuffle(playlists[playlistkey]._[directorymapkey])[0];
-        };
-    };
+        }
+    }
     return false;
 };
 
 rescan();
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer(function (req, res) {
     if (req.method.toUpperCase() !== 'GET') {
-        log(req.socket.remoteAddress+':'+req.socket.remotePort+' tried method '+req.method.toUpperCase());
+        log(req.socket.remoteAddress + ':' + req.socket.remotePort + ' tried method ' + req.method.toUpperCase());
         req.socket.destroy();
-    } else if(config.statuspage && config.statuspage.readable && config.statuspage.readable.path && req.url === config.statuspage.readable.path && ((in_array('*', config.statuspage.readable.allowedips) || in_array('0.0.0.0', config.statuspage.readable.allowedips) || in_array(req.socket.remoteAddress, config.statuspage.readable.allowedips)) || (in_array('*', config.statuspage.allowedips) || in_array('0.0.0.0', config.statuspage.allowedips) || in_array(req.socket.remoteAddress, config.statuspage.allowedips)))) {
+    } else if (config.statuspage && config.statuspage.readable && config.statuspage.readable.path && req.url === config.statuspage.readable.path && ((in_array('*', config.statuspage.readable.allowedips) || in_array('0.0.0.0', config.statuspage.readable.allowedips) || in_array(req.socket.remoteAddress, config.statuspage.readable.allowedips)) || (in_array('*', config.statuspage.allowedips) || in_array('0.0.0.0', config.statuspage.allowedips) || in_array(req.socket.remoteAddress, config.statuspage.allowedips)))) {
         res.writeHead(200, {
             'content-type': 'text/plain',
             'connection': 'close'
         });
         var uptime = process.uptime();
-        var systemload = Math.round(os.loadavg()[0]*100)+'% ('+os.loadavg().join(' ')+')';
+        var systemload = Math.round(os.loadavg()[0] * 100) + '% (' + os.loadavg().join(' ') + ')';
         var memoryheap = process.memoryUsage();
-            memoryheap = Math.round(memoryheap.heapUsed/memoryheap.heapTotal*100)+'%';
-        res.write(
-            'Uptime: '+uptime+'\n'+
-            'System load: '+systemload+'\n'+
-            'Memory heap: '+memoryheap+'\n'
-        );
+        memoryheap = Math.round(memoryheap.heapUsed / memoryheap.heapTotal * 100) + '%';
+        res.write('Uptime: ' + uptime + '\n' +
+                  'System load: ' + systemload + '\n' +
+                  'Memory heap: ' + memoryheap + '\n');
         res.end();
-    } else if(config.statuspage && config.statuspage.parseable && config.statuspage.parseable.path && req.url === config.statuspage.parseable.path && ((in_array('*', config.statuspage.parseable.allowedips) || in_array('0.0.0.0', config.statuspage.parseable.allowedips) || in_array(req.socket.remoteAddress, config.statuspage.parseable.allowedips)) || (in_array('*', config.statuspage.allowedips) || in_array('0.0.0.0', config.statuspage.allowedips) || in_array(req.socket.remoteAddress, config.statuspage.allowedips)))) {
+    } else if (config.statuspage &&
+               config.statuspage.parseable &&
+               config.statuspage.parseable.path &&
+               req.url === config.statuspage.parseable.path &&
+               ((in_array('*', config.statuspage.parseable.allowedips) ||
+                 in_array('0.0.0.0', config.statuspage.parseable.allowedips) ||
+                 in_array(req.socket.remoteAddress, config.statuspage.parseable.allowedips)) ||
+                (in_array('*', config.statuspage.allowedips) ||
+                 in_array('0.0.0.0', config.statuspage.allowedips) ||
+                 in_array(req.socket.remoteAddress, config.statuspage.allowedips)))) {
         res.writeHead(200, {
             'content-type': 'application/json',
             'connection': 'close'
         });
-        res.end(JSON.stringify({'config':config,'playlists':playlists}));
-    } else if(config.statuspage && config.statuspage.inspect && config.statuspage.inspect.path && req.url === config.statuspage.inspect.path && ((in_array('*', config.statuspage.inspect.allowedips) || in_array('0.0.0.0', config.statuspage.inspect.allowedips) || in_array(req.socket.remoteAddress, config.statuspage.inspect.allowedips)) || (in_array('*', config.statuspage.allowedips) || in_array('0.0.0.0', config.statuspage.allowedips) || in_array(req.socket.remoteAddress, config.statuspage.allowedips)))) {
+        res.end(JSON.stringify({'config': config, 'playlists': playlists}));
+    } else if (config.statuspage &&
+               config.statuspage.inspect &&
+               config.statuspage.inspect.path &&
+               req.url === config.statuspage.inspect.path &&
+               ((in_array('*', config.statuspage.inspect.allowedips) ||
+                 in_array('0.0.0.0', config.statuspage.inspect.allowedips) ||
+                 in_array(req.socket.remoteAddress, config.statuspage.inspect.allowedips)) ||
+                (in_array('*', config.statuspage.allowedips) ||
+                 in_array('0.0.0.0', config.statuspage.allowedips) ||
+                 in_array(req.socket.remoteAddress, config.statuspage.allowedips)))) {
         res.writeHead(200, {
             'content-type': 'application/json',
             'connection': 'close'
         });
-        res.end(util.inspect({'config':config,'playlists':playlists}, config.statuspage.inspect.options));
-    } else if(!playlists[req.url] || (!in_array('*', config.server.allowedips) && !in_array('0.0.0.0', config.server.allowedips) && !in_array(req.socket.remoteAddress, config.server.allowedips))) {
-        log(req.socket.remoteAddress+' tried to connect');
+        res.end(util.inspect({'config': config, 'playlists': playlists}, config.statuspage.inspect.options));
+    } else if (!playlists[req.url] ||
+               (!in_array('*', config.server.allowedips) &&
+                !in_array('0.0.0.0', config.server.allowedips) &&
+                !in_array(req.socket.remoteAddress, config.server.allowedips))) {
+        log(req.socket.remoteAddress + ' tried to connect');
         req.socket.destroy();
     } else {
-        var clientid = req.socket.remoteAddress+'_'+uniqid('', true);
+        var clientid = req.socket.remoteAddress + '_' + uniqid('', true);
         var playlistkey = req.url;
         var connecttime = new Date();
-        log(clientid+' connected to playlist '+playlistkey);
+        log(clientid + ' connected to playlist ' + playlistkey);
         res.sendDate = false;
-        res.writeHead(200, {'connection':'close'});
+        res.writeHead(200, {'connection': 'close'});
         var file = getrandfile(playlistkey);
-        if(!file) {
-            log(clientid+' disconnected from playlist '+playlistkey+' after '+(((new Date)-connecttime)/1000)+'s by no file event');
+        if (!file) {
+            log(clientid + ' disconnected from playlist ' + playlistkey + ' after ' + (((new Date()) - connecttime) / 1000) + 's by no file event');
             res.end();
         } else {
-            log(clientid+' gets '+file);
-            var decoder = spawn(config.global.converterpath, [
+            log(clientid + ' gets ' + file);
+            var decoder = child_process.spawn(config.global.converterpath, [
                 '-loglevel', 'warning',
                 '-analyzeduration', '5000',
                 '-re',
@@ -228,26 +243,28 @@ var server = http.createServer(function(req, res) {
                 '-sn', '-vn',
                 'pipe:1'
             ]);
-            decoder.stderr.on('data', function(chunk) {
+            decoder.stderr.on('data', function (chunk) {
                 process.stderr.write(chunk);
             });
-            res.once('close', function() {
-                log(clientid+' disconnected from playlist '+playlistkey+' after '+(((new Date)-connecttime)/1000)+'s by client close event');
+            res.once('close', function () {
+                log(clientid + ' disconnected from playlist ' + playlistkey + ' after ' + (((new Date()) - connecttime) / 1000) + 's by client close event');
                 decoder.kill();
             });
-            decoder.stdout.once('close', function() {
-                log(clientid+' disconnected from playlist '+playlistkey+' after '+(((new Date)-connecttime)/1000)+'s by decoder end event');
+            decoder.stdout.once('close', function () {
+                log(clientid + ' disconnected from playlist ' + playlistkey + ' after ' + (((new Date()) - connecttime) / 1000) + 's by decoder end event');
                 res.end();
             });
             decoder.stdout.pipe(res, {end: false});
-        };
-    };
+        }
+    }
 });
 
 server.listen(config.server.port, config.server.ip);
 
-if(typeof config.global.rescaninterval === 'number' && config.global.rescaninterval >= 0 && parseInt(config.global.rescaninterval) === config.global.rescaninterval) {
-    setInterval(function() {
+if (typeof config.global.rescaninterval === 'number' &&
+    config.global.rescaninterval >= 0 &&
+    parseInt(config.global.rescaninterval, 0) === config.global.rescaninterval) {
+    setInterval(function () {
         rescan();
     }, config.global.rescaninterval);
-};
+}
