f9442da14d466113f461cf0a51e2f342282ff50b
Fusl <root@hallowe.lt>
2013-11-21 04:20:11 +0100

    omg i forgot to add functions.js -.-

---
+++ functions.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/*jslint
+   browser: false, devel: false, node: true, rhino: false, passfail: false,
+   bitwise: false, debug: false, eqeq: false, evil: false, forin: false,
+   newcap: false, nomen: true, plusplus: true, regexp: false, unparam: false,
+   sloppy: false, stupid: false, sub: false, vars: false, white: false,
+   indent: 4, maxlen: 256
+*/
+
+var functions = {};
+module.exports = functions;
+
+functions.child_process = require('child_process');
+functions.fs = require('fs');
+functions.http = require('http');
+functions.os = require('os');
+functions.path = require("path");
+functions.util = require('util');
+
+functions.exec = functions.child_process.exec;
+functions.spawn = functions.child_process.spawn;
+
+functions.in_array = function (needle, haystack) {
+    if (!(haystack instanceof Array)) {
+        return false;
+    }
+    if (haystack.indexOf(needle) === -1) {
+        return false;
+    }
+    return true;
+};
+
+functions.log = function (msg) {
+    if (typeof msg === 'undefined') {
+        return;
+    }
+    functions.util.log(msg);
+};
+
+functions.trimnl = function (input) {
+    return input.toString().replace(/(\r\n|\r|\n|\n\r)/, '');
+};
+
+functions.uniqid = function () {
+    return Math.random().toString(16) + Math.random().toString(16);
+};
+
+functions.icystring = function (obj) {
+    var s = [];
+    Object.keys(obj).forEach(function (key) {
+        s.push(key);
+        s.push('=\'');
+        s.push(obj[key]);
+        s.push('\';');
+    });
+    return s.join('');
+};
+
+functions.shuffle = function (inputArr) {
+    if (!(inputArr instanceof Array)) {
+        return false;
+    }
+    var outputArr = [],
+        k;
+    for (k in inputArr) {
+        if (inputArr.hasOwnProperty(k)) {
+            outputArr.push(inputArr[k]);
+        }
+    }
+    return outputArr.sort(function () {
+        return 0.5 - Math.random();
+    });
+};
